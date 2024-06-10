c02ffaddf2786399997e5b7429a03e9be3125371
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-10 23:05:10 +0530

    employeeappdesin3

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Search from './component/Search';
 import Delete from './component/Delete';
 import ViewAll from './component/ViewAll';
 import Nav from './component/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <AddEmployee/>
-      <Search/>
-      <Delete/>
-      <ViewAll/>
-      <Nav/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AddEmployee />} />
+          <Route path='/Search' element={<Search/>} />
+          <Route path='/Delete' element={<Delete />} />
+          <Route path='/ViewAll' element={<ViewAll />} />
+          <Route path='/Nav' element={<Nav />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-    
+
   );
 }
 
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div>
        <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="AddEmployee.jsx">Home</a>
+    <Link class="navbar-brand" to="/">Home</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="Search.jsx">Search</a>
+          <Link class="nav-link active" aria-current="page" to="/Search">Search</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="Delete.jsx">Delete</a>
+          <Link class="nav-link" to="/Delete">Delete</Link>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             ViewAll
-          </a>
+          </Link>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="ViewAll.jsx">View</a></li>
+            <li><Link class="dropdown-item" to="/ViewAll">View</Link></li>
           </ul>
         </li>
       </ul>
