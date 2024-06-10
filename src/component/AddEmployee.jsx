95133941ccd5a5370265c5941db1a03c8c19196a
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-10 21:14:43 +0530

    employeeappdesin1

---
+++ src/component/AddEmployee.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AddEmployee = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"></div>
+                </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddEmployee
